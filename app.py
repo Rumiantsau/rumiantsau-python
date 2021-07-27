264066b44798a069a0fc4693c78e6e96182026d9
Mikalai <mikalairumiantsau@gmail.com>
2021-07-27 10:11:05 +0300

    Updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def greeting():
-    return "<h1 style='color:yellow'>Hello World 1</h1>"
+    return "Rumiantsau-python-app. Hello World 1"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8181)
